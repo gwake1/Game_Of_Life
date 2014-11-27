2956c924d13b3303e29567f83430b17b2dd7e14d
Gerald A. Wakefield <gwake1@gmail.com>
2014-11-27 11:46:32 -0600

    Refactor generateGrid()

--- spec/main.js
+++ spec/main.js
@@ -7,36 +7,24 @@ document.querySelector(".btn").addEventListener("click", function(){
 })
 
 
-
-var rows = 25;
-var cells = 25;
-
+var matrix = {
+  rows: 25,
+  cells: 25,
+}
+// // 1. append rows and cells
 function generateGrid(){
-  x = rows;
-  y = cells;
-  for(var i=0; i< x; i++){
+  for(var i=0; i< matrix.rows; i++){
     var table = document.querySelector(".life");
     var tr = document.createElement("tr");
     table.appendChild(tr);
-    for(var j=0; j< y; j++){
+    for(var j=0; j< matrix.cells; j++){
       var td = document.createElement("td");
       tr.appendChild(td);
     }
   }
-  return "create table rows";
+  console.log("calling generateGrid(): made a "+matrix.rows+"x"+matrix.cells+" table");
 }
-//
-// // 1. append rows and cells
-// var table = document.querySelector(".life");
-// // Create an empty <tr> element and add it to the 1st position of the table:
-// var row = table.insertRow(0);
-// // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-// var cell1 = row.insertCell(0);
-// var cell2 = row.insertCell(1);
-// // Add some text to the new cells:
-// cell1.innerHTML = "NEW CELL1";
-// cell2.innerHTML = "NEW CELL2";
-//
 // // 2. Generate random matrix
+
 // // 3. Count Cells living nearby
 // // 4. Apply rules
